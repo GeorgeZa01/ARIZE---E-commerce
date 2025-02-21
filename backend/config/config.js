bfff53c6274ec99eae9675456c5d5f391c6754e3
GerogeZa01 <jemaile75@gmail.com>
2025-02-21 18:00:47 +0200

    update to forgot password

--- backend/config/config.js
+++ backend/config/config.js
@@ -4,9 +4,9 @@ import { config } from 'dotenv';
 config();
 
 const pool = mysql.createPool({
-    host: process.env.HOST || "localhost",
+    host: process.env.HOST || "127.0.0.1",
     user: process.env.USER || "root",
-    password: process.env.PASSWORD || "Ursw!nF@r0",
+    password: process.env.PASSWORD || "Macaws01",
     database: process.env.DATABASE || "arize_db",
     waitForConnections: true,
     connectionLimit: 10,
