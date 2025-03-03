c808fc2df03da42bb5679e7570577c92f7c9a702
Nasra-Hendricks <Nasrahen@gmail.com>
2025-03-03 15:46:48 +0200

    added cookies,cookie policy and fixed small errors

--- backend/routes/cartRouter.js
+++ backend/routes/cartRouter.js
@@ -1,11 +1,11 @@
-import express from "express"
-const router = express.Router();
-import {addToCartCon,getCartItemsCon,removeFromCartCon,increaseQuantityCon} from '../controller/cartController.js';
+// import express from "express"
+// const router = express.Router();
+// import {addToCartCon,getCartItemsCon,removeFromCartCon,increaseQuantityCon} from '../controller/cartController.js';
 
-router.post('/', addToCartCon);
-router.get('/:userId', getCartItemsCon);
-router.delete('/:cartId', removeFromCartCon);
-router.put('/increase/:cartId', increaseQuantityCon);
+// router.post('/', addToCartCon);
+// router.get('/:userId', getCartItemsCon);
+// router.delete('/:cartId', removeFromCartCon);
+// router.put('/increase/:cartId', increaseQuantityCon);
 
-export default router
+// export default router
 
