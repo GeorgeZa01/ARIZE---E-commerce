eb8b75002c182a12e19a53af57a32c79b8089c88
AvelaNikani <avelarnikani47@gmail.com>
2025-03-05 13:44:09 +0200

    updated the checkout page set up and the filtered pages

--- backend/routes/cartRouter.js
+++ backend/routes/cartRouter.js
@@ -12,7 +12,7 @@ const router = express.Router();
 router.get('/:userId', getCartItemsCon); // Get cart items
 router.post('/add', handleAddToCart); // Add to cart
 router.delete('/delete/:cartId', handleRemoveFromCart); // Remove item
-router.patch('/update/increase/:cartId', handleIncreaseQuantity); // Increase quantity
-router.patch('/update/decrease/:cartId', handleDecreaseQuantity); // Decrease quantity
+router.put('/update/increase/:cartId', handleIncreaseQuantity); // Increase quantity
+router.put('/update/decrease/:cartId', handleDecreaseQuantity); // Decrease quantity
 
 export default router;
